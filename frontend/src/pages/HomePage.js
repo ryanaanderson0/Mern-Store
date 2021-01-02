e4d73293ee864ac60b7182129845853b0dfa9fcf
Ryan Anderson <ryanaanderson0@gmail.com>
2021-01-02 11:17:35 -0800

    added reducer to homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,64 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col} from 'react-bootstrap'
-import Product from '../components/Product'
-import '../App.css'
-import MastHead from '../components/MastHead'
-import About from '../components/About'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import "../App.css";
+import MastHead from "../components/MastHead";
+import About from "../components/About";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/prouductActions";
 
+const HomePage = () => {
+  const dispatch = useDispatch();
 
-const HomePage = ({ match}) => {
+  const productList = useSelector((state) => state.productList)
 
-    // useState react hook to initilize products as an empty array
-    const [products, setProducts] = useState([])
+  //destructure data we want from the state (loading boolean, error, and products)
+  const { loading, error, products } = productList
 
+  //useEffect react hook, make GET request after HomePage component is rendered
+  useEffect(() => {
+      dispatch(listProducts())
+    }, [dispatch]
+  )
 
-    //useEffect react hook, make GET request after HomePage component is rendered
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
+  return (
+    <div style={{ backgroundColor: "#F4F1DE" }}>
+      <MastHead />
 
-            setProducts(data)
-        }
-        fetchProducts()
-    }, 
-    // fires off when useEffect is changed
-    [])
+      <h1 style={{ paddingTop: "7%" }}>Latest Releases</h1>
 
-    return (
-        <div style={{backgroundColor: "#F4F1DE"}}>
+      {/* //loading and error conditional */}
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row className="pl-3">
+          {/* map all the product arrays contained in the products.js file and return in columns  */}
+          {products.map((products) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              {/* pass in the products.map to Product component */}
+              <Product products={products} />
+            </Col>
+          ))}
+          ;
+        </Row>
+      )}
 
-          <MastHead />
+      <section className="pt-5 pb-5">
+        <h1 className="pb-3">
+          <strong>Don't Drink Alone</strong>
+        </h1>
 
-          <h1 style={{paddingTop: '7%'}}>Latest Releases</h1> 
-          <Row className="pl-3">
-            {/* map all the product arrays contained in the products.js file and return in columns  */}
-            {products.map(products => (
-                <Col sm={12} md={6} lg={4} xl={3}>
-                    {/* pass in the products.map to Product component */}
-                    <Product products={products}/>
-                </Col>
-            ))};
-          </Row>
-        
-         <section className="pt-5 pb-5">
-             <h1 className="pb-3"><strong>Don't Drink Alone</strong></h1>
+        <Link>
+          <button className="primary_button">Join Now</button>
+        </Link>
 
-             <Link>
-                <button className="primary_button">Join Now</button>
-             </Link>
-             
-             <h4 className="pt-3">Join Now & Get 10% Off Your First Order</h4>
-         </section>
+        <h4 className="pt-3">Join Now & Get 10% Off Your First Order</h4>
+      </section>
 
-          <section className="spikes_tan">
-          </section>
+      <section className="spikes_tan"></section>
 
-          <About />
-               
-        </div>
-    )
-}
+      <About />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
